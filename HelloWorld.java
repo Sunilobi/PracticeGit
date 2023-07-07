51ae262529f147a22fb591f1e6d933aefb7beb8d
Sunilobi <106995530+Sunilobi@users.noreply.github.com>
2023-07-07 12:00:18 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,8 @@
-public class HelloWorld {
+package com.example;
+
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Your time is being valued.");
+        System.out.println("You are seeing the demonstration of a pipeline built using Jenkins, Git, JFrog Artifactory, Maven, SonarQube, Docker, and Tomcat.");
     }
 }
-
